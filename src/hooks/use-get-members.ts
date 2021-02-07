582d774ed182bd8c62e69c8140d532327b63e789
kazuhi-ra <kazuhira4126@gmail.com>
2021-02-08 00:38:33 +0900

    カスタムhookにReduxを組み込む

--- src/hooks/use-get-members.ts
+++ src/hooks/use-get-members.ts
@@ -1,19 +1,27 @@
 import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
+import { UserState, userSlice } from 'features/user'
 import { getMembers } from 'domains/github/services/get-members'
 import { User } from 'domains/github/models/user'
 import { OrgCode } from 'data/org-codes'
 
 export const useGetMembers = (orgCode: OrgCode): [User[], boolean] => {
-  const [users, setUsers] = useState<User[]>([])
+  const dispatch = useDispatch()
+  const users = useSelector<UserState, User[]>((state) => state.users)
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
+    let isUnmounted = false
+    const { membersGotten } = userSlice.actions
+
     const load = async () => {
       setIsLoading(true)
       try {
         const usersData = await getMembers(orgCode)
-        setUsers(usersData)
+        if (!isUnmounted) {
+          dispatch(membersGotten({ users: usersData }))
+        }
       } catch (error) {
         throw new Error(`organization ${orgCode} not exists`)
       } finally {
@@ -22,7 +30,11 @@ export const useGetMembers = (orgCode: OrgCode): [User[], boolean] => {
     }
 
     load()
-  }, [orgCode])
+
+    return () => {
+      isUnmounted = true
+    }
+  }, [dispatch, orgCode])
 
   return [users, isLoading]
 }
