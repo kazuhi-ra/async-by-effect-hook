95894f413f32a2b7d7bcb966dced1a8d82129373
kazuhi-ra <kazuhira4126@gmail.com>
2021-02-07 23:30:54 +0900

    Sliceを作成

---
+++ src/features/user.ts
@@ -0,0 +1,19 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { User } from 'domains/github/models/user'
+
+type UserState = { users: User[] }
+const initialState: UserState = { users: [] }
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    membersGotten: (state, action: PayloadAction<UserState>) => ({
+      ...state,
+      users: action.payload.users,
+      isLoading: false,
+      error: null,
+    }),
+  },
+})
