ca8697e5f9b35bf09d960ae769b24e662aeca8aa
kazuhi-ra <kazuhira4126@gmail.com>
2021-02-07 23:31:10 +0900

    Storeを仕込む

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,21 @@
 import ReactDOM from 'react-dom'
-import './index.css'
+import { Provider } from 'react-redux'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+
+import { userSlice } from 'features/user'
+
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import './index.css'
+
+const store = configureStore({
+  reducer: userSlice.reducer,
+  middleware: [...getDefaultMiddleware({ thunk: false })],
+})
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
