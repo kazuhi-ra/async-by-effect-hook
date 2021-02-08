e1cf43bb70c6abbbe6568196aeb9e7d8cd4b18a8
kazuhi-ra <kazuhira4126@gmail.com>
2021-02-09 00:40:57 +0900

    storeから読み取ったデータを表示

--- src/containers/Pages/Members.tsx
+++ src/containers/Pages/Members.tsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 import { Members } from 'components/Pages/Members'
+import { getMembersStarted } from 'features/user/user-actions'
+import { UserState } from 'features/user/user-reducer'
+import { User } from 'domains/github/models/user'
+
+const orgCode = 'apple'
 
 export const EnhancedMembers = () => {
+  const dispatch = useDispatch()
+  const users = useSelector<UserState, User[]>((state) => state.users)
+  const isLoading = useSelector<UserState, boolean>((state) => state.isLoading)
+
+  useEffect(() => {
+    dispatch(getMembersStarted(orgCode))
+  }, [dispatch])
   
   return (
-    <Members />
+    <Members users={users} isLoading={isLoading} />
   )
 }
