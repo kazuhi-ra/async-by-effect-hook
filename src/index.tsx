bf876b3c003a9fdd6e40a3caafad322d63abae4c
kazuhi-ra <kazuhira4126@gmail.com>
2021-02-09 00:40:11 +0900

    store作った

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,26 @@
 import ReactDOM from 'react-dom'
-import './index.css'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { userReducer } from 'features/user/user-reducer'
+import './index.css'
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(userReducer, composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
